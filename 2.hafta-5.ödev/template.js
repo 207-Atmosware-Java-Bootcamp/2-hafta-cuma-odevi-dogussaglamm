b3ea15e4042d9ac707ce6d5b8d2d0f39cccf8672
Doğuş Sağlam <dogussaglam@hotmail.com>
2022-06-03 17:12:48 +0300

    2.hafta-5.ödev

---
+++ 2.hafta-5.ödev/template.js
@@ -0,0 +1,19 @@
+// sifre sorgulama
+
+let temp = document.getElementById('message');
+
+var go =true;
+let passwordInput = prompt("enter your password","");
+while(go){
+let passwordInput2 = prompt("re-enter your password","");
+if(passwordInput===passwordInput2){
+    go=false;
+    alert("şifre dogru");
+    }
+    else{
+  
+        alert("şifre yanlış");
+    }  
+
+
+}
